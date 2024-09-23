956f0f66e71ea2f54b2635a5004ffd596bdbd724
reihaneh abbasi <reyhaneeab@gmail.com>
2024-09-23 21:37:05 +0200

    #3 Add Post endpoint for creating skills

--- server/controllers/skills.js
+++ server/controllers/skills.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 const Skill = require('../models/skill'); 
+const User = require('/models/user');
+
 
 // GET endpoint to retrieve all skills
 router.get('/v1/skills', (req, res, next) => {
@@ -23,6 +25,32 @@ router.post('/v1/skills', async (req, res, next) => {
         var skill = new Skill(req.body);
         await skill.save();
         res.status(201).json(skill);
+    } catch (error) {
+        next(error);
+    }
+});
+//POST endpoint for creating new skills
+router.post('/v1/skills', async (req, res, next) => {
+
+    const { name, level, category, userId, isAnInterest } = req.body;
+    try {
+        const user = await User.findById(userId);
+        const newSkill = new Skill({
+            name,
+            level,
+            category,
+            user: userId,
+            isAnInterest
+        });
+
+        const savedSkill = await newSkill.save();
+        if (isAnInterest) {
+            user.interests.push(savedSkill._id);
+        } else {
+            user.skills.push(savedSkill._id);
+        }
+        await user.save();
+        res.status(201).json(savedSkill);
     } catch (error) {
         next(error);
     }
