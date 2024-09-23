ef061b710f9ca298f6dd6ca6d3cdf560ddc62738
rawanah <rawanah@student.chalmers.se>
2024-09-23 22:22:06 +0200

    #3 Fix test breaking code

--- server/controllers/skills.js
+++ server/controllers/skills.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const Skill = require('../models/skill'); 
-const User = require('/models/user');
+const User = require('../models/user');
 
 
 // GET endpoint to retrieve all skills
