34a82673868e4bf11e55ee4ec7b0d57acde4383f
Bimnet Tesfamariam <gustesbi@student.gu.se>
2024-10-02 11:14:17 +0200

    #4 Add skeleton structure for home page & create other pages

--- client/src/router.js
+++ client/src/router.js
@@ -1,9 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from './views/Home.vue'
+import SignUp from './views/SignUp.vue'
+import Chatroom from './views/Chatroom.vue'
+import Search from './views/Search.vue'
+import Profile from './views/Profile.vue'
 
 const routes = [
-  { path: '/', name: 'home', component: Home }
+  { path: '/', name: 'home', component: Home },
+  { path: '/signup', name: 'Sign Up', component: SignUp },
+  { path: '/chatrooms', name: 'Chatroom', component: Chatroom },
+  { path: '/search', name: 'Search', component: Search },
+  { path: '/profile', name: 'Profile', component: Profile },
 ]
 
 const router = createRouter({
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div>
     <b-container fluid>
-      <h1 class="display-5 fw-bold">DIT342 Frontend</h1>
-      <p class="fs-4">Welcome to your DIT342 Frontend Vue.js App</p>
-      <b-button class="btn_message" variant="primary" v-on:click="getMessage()" >Get Message from Server</b-button>
-      <p class="col-xl-9">Message from the server:<br/>
-      {{ message }}</p>
+      <h1 class="display-5 fw-bold">LessonMatch</h1>
+      <SignUpButton />
     </b-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+
 import { Api } from '@/Api'
+import SignUpButton from '@/components/SignUpButton.vue'
 
 export default {
   name: 'home',
@@ -22,15 +20,10 @@ export default {
     }
   },
   methods: {
-    getMessage() {
-      Api.get('/')
-        .then(response => {
-          this.message = response.data.message
-        })
-        .catch(error => {
-          this.message = error
-        })
-    }
+    
+  },
+  components: {
+    SignUpButton
   }
 }
 </script>
