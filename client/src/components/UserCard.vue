47458176a3e3a26f8a18117f14a4001c3157c0e8
Bimnet Tesfamariam <gustesbi@student.gu.se>
2024-10-02 12:07:00 +0200

    #4 Add category dropdown to search page

---
+++ client/src/components/UserCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'UserCard',
+    
+}
+</script>
+
+
+<style scoped>
+</style>
--- client/src/views/Search.vue
+++ client/src/views/Search.vue
@@ -1,10 +1,38 @@
 <template>
     <div>
-        
+        <CategoryDropdownBar @category-selected="searchCategory"/>
     </div>
+    <div v-if="this.selectedCategory">Results for {{ selectedCategory }}:</div>
 </template>
 
 <script>
+import CategoryDropdownBar from '@/components/CategoryDropdownBar.vue'
+import UserCard from '@/components/UserCard.vue'
+
+import axios from 'axios'
+
+export default {
+    name: 'Search',
+
+    components: {
+        CategoryDropdownBar
+    },
+
+    data() {
+        return {
+            selectedCategory: '',
+            users: []
+        }
+    },
+
+    methods: {
+        async searchCategory(category) {
+            this.selectedCategory = category;
+            // await the category endpoint here
+            this.user
+        }
+    },
+}
 </script>
 
 
