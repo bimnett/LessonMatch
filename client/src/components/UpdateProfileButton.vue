3136f123494d464ef2827744c461e4b4fecbccad
reihaneh abbasi <reyhaneeab@gmail.com>
2024-10-03 22:57:39 +0200

    #4 change update profile button route to Profile

--- client/src/components/UpdateProfileButton.vue
+++ client/src/components/UpdateProfileButton.vue
@@ -1,40 +1,30 @@
 <template>
-    <div class="update-profile-button">
-      <b-button @click="navigateToUpdate" variant="primary" class="update-button">
-        Update Profile
-      </b-button>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    methods: {
-      navigateToUpdate() {
-        this.$router.push('/update-profile');
-      }
+  <div>
+    <b-button @click="navigateToProfile" variant="link">Edit Profile</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    navigateToProfile() {
+      this.$router.push('/profile');
     }
   }
-  </script>
-  
-  <style scoped>
-  .update-profile-button {
-    text-align: center;
-    margin-top: 20px;
-  }
-  
-  .update-button {
-    background-color: #6200ea;
-    color: #ffffff;
-    border: none;
-    border-radius: 4px;
-    padding: 10px 20px;
-    font-size: 16px;
-    cursor: pointer;
-    transition: background-color 0.3s ease;
-  }
-  
-  .update-button:hover {
-    background-color: #3700b3;
-  }
-  </style>
-  
+};
+</script>
+
+<style scoped>
+.b-button {
+  color: #6200ea;
+  text-decoration: underline;
+  cursor: pointer;
+  padding: 0;
+  border: none;
+  background: none;
+}
+
+.b-button:hover {
+  color: #3700b3;
+}
+</style>
