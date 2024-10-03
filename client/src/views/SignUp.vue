faa29d4d5e9d4d27305cb87108014ec11b905fa7
Bimnet Tesfamariam <gustesbi@student.gu.se>
2024-10-03 08:51:56 +0200

    #4 Add Sign up form to sign up page

--- client/src/views/SignUp.vue
+++ client/src/views/SignUp.vue
@@ -1,10 +1,36 @@
 <template>
     <div>
-        
+        <SignUpForm @form-data="postNewUser" />
+        <div v-if="formData.name">
+            {{ formData }}
+        </div>
     </div>
 </template>
 
 <script>
+import SignUpForm from '@/components/SignUpForm.vue'
+
+export default {
+    name: 'SignUp',
+    components: {
+        SignUpForm
+    },
+    data() {
+        return {
+            formData: {
+                name: '',
+                email: '',
+                password: ''
+            }
+        }
+    },
+    methods: {
+        async postNewUser(formData){
+            // Send request to create a new user
+            this.formData = formData;
+        }
+    }
+}
 </script>
 
 
