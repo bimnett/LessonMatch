9c5ae55cda25e34c2d421c27d3506e434c28d57a
reihaneh abbasi <reyhaneeab@gmail.com>
2024-10-09 14:07:02 +0200

    #4 Reverse the change on importing axios in api.js

--- client/src/Api.js
+++ client/src/Api.js
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from 'axios'
+import axios from 'axios'
 
 export const Api = axios.create({
   baseURL: import.meta.env.VITE_API_ENDPOINT || 'http://localhost:3000/api'
