c537701b85338afa2ad61becc368758caeaf4693
reihaneh abbasi <reyhaneeab@gmail.com>
2024-10-18 18:20:38 +0200

    #5 Display messages in private chatroom with user-specific alignment

--- client/src/views/PrivateChat.vue
+++ client/src/views/PrivateChat.vue
@@ -1,9 +1,11 @@
 <template>
     <div v-if="userId">
-      <div v-for="message in messages" :key="message._id">
-        <p>{{ message.sender.name }}: {{ message.content }}</p>
+      <div class="message-container">
+      <div v-for="message in messages" :key="message._id" :class="messageClass(message)">
+        <p class="message-content">{{ message.sender.name }}: {{ message.content }}</p>
+     <p class="message-timestamp">{{ formatTimestamp(message.sentAt) }}</p>
       </div>
-
+      </div>
       <ChatInput :chatroomId="chatroomId" @send-message="sendMessage"/>
     </div>
     <div v-else>
@@ -24,7 +26,7 @@ export default {
     return {
       messages: [],
       userId: localStorage.getItem('userId'),
-      chatroomId: this.$route.params.id
+      chatroomId: this.$route.params.chatroomId
     }
   },
   async mounted() {
@@ -62,8 +64,21 @@ export default {
       } catch (error) {
         console.error('Error sending message:', error)
       }
+    },
+
+    messageClass(message) {
+      return {
+        messageBox: true,
+        sent: message.sender._id === this.userId,
+        recived: message.sender._id !== this.userId
+      }
+    },
+    formatTimestamp(timestamp) {
+      const date = new Date(timestamp)
+      return date.toLocaleString()
     }
   },
+
   beforeDestroy() {
     if (this.userId) {
       socket.off('message')
@@ -72,3 +87,39 @@ export default {
   }
 }
 </script>
+<style scoped>
+.messages-container {
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  max-height: 80vh;
+  overflow-y: auto;
+}
+
+.message-box {
+  padding: 10px;
+  margin: 5px 0;
+  border-radius: 10px;
+  max-width: 60%;
+}
+
+.sent {
+  align-self: flex-end;
+  background-color: #dcf8c6;
+}
+
+.received {
+  align-self: flex-start;
+  background-color: #f1f0f0;
+}
+
+.message-content {
+  margin: 0;
+}
+
+.message-timestamp {
+  font-size: 0.8em;
+  text-align: right;
+  color: #666;
+}
+</style>
