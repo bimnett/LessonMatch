0b28495e62d37ff145dc6f5e12cd4c5448dd1a3f
rawanah <rawanah@student.chalmers.se>
2024-10-21 22:22:47 +0200

    #6 Fix position of avatar and change page name

--- client/src/components/Menu/DesktopMenu.vue
+++ client/src/components/Menu/DesktopMenu.vue
@@ -8,12 +8,12 @@
         <div v-if="userId" class="nav-group">
           <router-link class="nav-link" to="/search">Search</router-link>
           <router-link class="nav-link" :to="`/profile/${userId}`">Profile</router-link>
-          <router-link class="nav-link" :to="`/chatrooms/${userId}`">Chatroom</router-link>
+          <router-link class="nav-link" :to="`/chatrooms/${userId}`">Chatrooms</router-link>
         </div>
         <div v-else class="nav-group">
           <router-link class="nav-link" to="/signin">Search</router-link>
           <router-link class="nav-link" to="/signin">Profile</router-link>
-          <router-link class="nav-link" to="/signin">Chatroom</router-link>
+          <router-link class="nav-link" to="/signin">Chatrooms</router-link>
         </div>
       </div>
     </div>
